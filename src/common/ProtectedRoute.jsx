d0d168be0a23264e916312cecda102b9ecdaef93
Shivamtiwari2506 <shivamtiwari0784@gmail.com>
2025-11-16 16:12:18 +0530

    login logic updated and route fixed

--- src/common/ProtectedRoute.jsx
+++ src/common/ProtectedRoute.jsx
@@ -1,43 +1,12 @@
-import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
-import toast from "react-hot-toast";
 import Loader from "../utils/Loader";
-import {jwtDecode} from "jwt-decode";
 
 const ProtectedRoute = ({ children }) => {
-  const [status, setStatus] = useState("checking");
+  const token = localStorage.getItem("accessToken");
 
-  useEffect(() => {
-    const token = localStorage.getItem("accessToken");
-
-    if (!token) {
-      setStatus("unauthorized");
-      return;
-    }
-
-    try {
-      const { exp } = jwtDecode(token);
-
-      // If token expired
-      if (Date.now() >= exp * 1000) {
-        setStatus("unauthorized");
-      } else {
-        setStatus("authorized");
-      }
-    } catch (error) {
-      console.error("Invalid token:", error);
-      setStatus("unauthorized");
-    }
-  }, []);
-
-  useEffect(() => {
-    if (status === "unauthorized") {
-      toast.error("Session expired. Please log in again.");
-    }
-  }, [status]);
-
-  if (status === "checking") return <Loader />;
-  if (status === "unauthorized") return <Navigate to="/login" replace />;
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
 
   return children;
 };
--- src/utils/Loader.jsx
+++ src/utils/Loader.jsx
@@ -1,20 +1,13 @@
-import React from "react";
-import { Spin } from "antd";
 import { IconLoader2 } from "@tabler/icons-react";
 
 const Loader = () => {
-  const customIcon = (
-    <IconLoader2
-      size={40}
-      stroke={2.5}
-      className="text-gray-800"
-      
-    />
-  );
-
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-white space-y-4">
-      <Spin indicator={customIcon} spinning/>
+      <IconLoader2
+        size={40}
+        stroke={2.5}
+        className="text-black animate-spin"
+      />
       <p className="text-gray-600 text-lg font-medium">Loading, please wait...</p>
     </div>
   );
